ca0262de1ddf3a4055f0e220d9f512129be55d37
ybouchrit2 <ybouchrit2@gmail.com>
2025-10-10 18:54:20 +0200

    Serve main site at '/': forward to static index.html instead of 'ok'

--- admin-server/src/main/java/com/aversa/admin/controller/RootController.java
+++ admin-server/src/main/java/com/aversa/admin/controller/RootController.java
@@ -1,12 +1,12 @@
 package com.aversa.admin.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
     @GetMapping("/")
     public String root() {
-        return "ok";
+        return "forward:/index.html";
     }
 }
