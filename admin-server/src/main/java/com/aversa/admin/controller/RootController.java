4c89cb2bea682ffa48d63904675bc002d05abfc0
ybouchrit2 <ybouchrit2@gmail.com>
2025-10-12 17:53:55 +0200

    تهيئة خدمات Render: إضافة affidaro-site للنطاقات affidaro.com وwww.affidaro.com وضبط النشر من static؛ تعيين admin.affidaro.com لخدمة الأدمن؛ تغيير جذر خدمة الأدمن لعرض /admin/login.html بدل الصفحة العامة

--- admin-server/src/main/java/com/aversa/admin/controller/RootController.java
+++ admin-server/src/main/java/com/aversa/admin/controller/RootController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RootController {
     @GetMapping("/")
     public String root() {
-        return "forward:/index.html";
+        // Serve admin login as the root page on the admin service
+        return "forward:/admin/login.html";
     }
 }
